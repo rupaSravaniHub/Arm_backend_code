0247b93abd096195676b0c7b82f9289e3dad47aa
rupaSravaniHub <sravani020809@gmail.com>
2025-02-21 16:37:52 +0530

    Update ReplayRoute.java

--- src/main/java/com/route/ReplayRoute.java
+++ src/main/java/com/route/ReplayRoute.java
@@ -4,23 +4,38 @@ import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ReplayRoute extends RouteBuilder {
+public class ReplayRoute extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
-		from("direct:Replay")	
-				.marshal().json()
-				.setHeader("originalMessageId", jsonpath("$.messageId"))
-				.setHeader("scenarioId", jsonpath("$.flowId")).setHeader("scenarioName", jsonpath("$.flowName"))
-				.setHeader("countryCode", jsonpath("$.region")).setHeader("hops", jsonpath("$.stages"))
-				.setHeader("inboundQueue", jsonpath("$.inboundQueue"))
-				.setHeader("outboundQueue", jsonpath("$.outboundQueue")).setHeader("brokerurl", jsonpath("$.brokerurl"))
-				 .setHeader("transdata", jsonpath("$.transdata"))
-				.setHeader("exceptionRoute", jsonpath("$.exceptionRoute")).setBody(jsonpath("$.payload"))
-				.log("MQ camel ${body} ---> ${header.exceptionRoute.toUpperCase()}")
-				.toD("activemq:queue:${header.exceptionRoute.toUpperCase()}?replyTo=direct:replayStatus");
-
-		from("direct:replayStatus").setBody().constant("Replay Queue Processed");
+		from("direct:Replay")
+//		from("activemq:queue:reply")
+		.routeId("ReplyRoute")
+		.marshal().json()
+		 .log("reply ${body}")
+		 	
+			.setHeader("messageId", jsonpath("$.messageId"))
+			.setHeader("scenarioId", jsonpath("$.flowId"))
+			.setHeader("scenarioName", jsonpath("$.flowName"))
+			.setHeader("countryCode", jsonpath("$.region"))
+		    .setHeader("hops", jsonpath("$.stages"))
+		    .setHeader("transdata", jsonpath("$.transdata"))
+		    .log("transdata ${header.transdata}")
+			.setHeader("inboundQueue", jsonpath("$.inboundQueue"))
+			.setHeader("outboundQueue", jsonpath("$.outboundQueue"))
+			.setHeader("brokerurl", jsonpath("$.brokerurl"))
+			.setHeader("exceptionRoute", jsonpath("$.exceptionRoute"))
+//			.setHeader("exitStatus", jsonpath("$.exitStatus"))
+			.setBody(jsonpath("$.payload"))
+			.validate(header("exceptionRoute").isNotNull())
+		.log("MQ  camel ${body} ---> ${header.exceptionRoute.toUpperCase()}")
+//		.to("activemq:queue:ttt")
+	        .toD("activemq:queue:${header.exceptionRoute.toUpperCase()}?replyTo=direct:replayStatus")
+	        .log("test ${body} ---> ${headers}")
+	     ;
+		
+		from("direct:replayStatus")
+        .setBody().constant("Replay Queue Processed");
 	}
 
 }
