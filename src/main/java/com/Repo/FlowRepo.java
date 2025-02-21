ebd64309853180fd90890d4c245a922592906827
rupaSravaniHub <sravani020809@gmail.com>
2025-02-21 16:40:19 +0530

    Update FlowRepo.java

--- src/main/java/com/Repo/FlowRepo.java
+++ src/main/java/com/Repo/FlowRepo.java
@@ -1,8 +1,8 @@
 package com.Repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.pojo.Flow;
 
@@ -14,4 +14,11 @@ public interface FlowRepo extends MongoRepository<Flow, String> {
 	Flow findByFlowIdAndFlowNameAndRegion(String FlowId, String flowName, String region);
 
 	Flow findByFlowId(String flowId);
+	
+	@Query("{ 'flowId': :#{#flowId}, 'flowName': :#{#flowName}, 'region': :#{#region} }")
+	Flow getVewModel(String flowId,String flowName, String region);
+
+	@Query("{ 'flowId': :#{#flowId}}")
+	Flow getByFlowId(String flowId);
+
 }
